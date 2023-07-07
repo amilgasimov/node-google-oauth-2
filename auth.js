594b275a7645842089f57d42e8ea7678a02882ae
amilgasimov <93605485+amilgasimov@users.noreply.github.com>
2023-07-07 22:33:20 +0400

    com

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_CLIENT_ID = "112725499131-46hgiioj2g7g943etk0vqatig6r2ck74.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback",
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
